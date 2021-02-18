01095c7d874c551dbf7e1226f230731a61c18b0e
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-02-18 19:47:01 +0100

    Made a script with multiple models

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
-import sklearn
 import pandas as pd
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -22,6 +21,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.linear_model import SGDClassifier
 from sklearn.base import clone
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
 
 
 # Load the data
@@ -29,17 +31,17 @@ df = pd.read_csv('Data_For_Model.csv')
 X = df.iloc[:, 1::].values
 y = df.iloc[:, 0].values
 # Split the data into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
-                                                    random_state=42)
-X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test,
-                                                              test_size=0.5,
-                                                              random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,
+#                                                     random_state=42)
+# X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test,
+#                                                               test_size=0.5,
+#                                                               random_state=42)
 
-sgd_classifier = SGDClassifier(random_state=42)
-sgd_classifier.fit(X_train, y_train)
-y_pred = sgd_classifier.predict(X_test)
-n_correct = sum(y_pred == y_test)
-print(n_correct / len(y_pred))
+# sgd_classifier = SGDClassifier(random_state=42)
+# sgd_classifier.fit(X_train, y_train)
+# y_pred = sgd_classifier.predict(X_test)
+# n_correct = sum(y_pred == y_test)
+# print(n_correct / len(y_pred))
 # sgd_classifier = SGDClassifier(random_state=42)
 
 # skfolds = StratifiedKFold(n_splits=10)
@@ -55,7 +57,8 @@ print(n_correct / len(y_pred))
 #     n_correct = sum(y_pred == y_test)
 #     print(n_correct / len(y_pred))
 
-print(cross_val_score(sgd_classifier, X_train, y_train, cv = 10, scoring = 'accuracy'))
+# print(cross_val_score(sgd_classifier, X_train, y_train,
+#       cv = 10, scoring = 'accuracy'))
 classifiers = [
     KNeighborsClassifier(3),
     SVC(probability=True),
@@ -69,13 +72,13 @@ classifiers = [
     LogisticRegression()]
 
 cols = ['Classifier', 'Accuracy', 'Precision', 'Recall', 'F1Score']
-log = pd.DataFrame(columns = cols)
+log = pd.DataFrame(columns=cols)
 acc_dict = {}
 prec_dict = {}
 rec_dict = {}
 f1_dict = {}
 
-skfolds = StratifiedKFold(n_splits = 10)
+skfolds = StratifiedKFold(n_splits=10)
 
 for train_index, test_index in skfolds.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
@@ -84,10 +87,10 @@ for train_index, test_index in skfolds.split(X, y):
         name = clf.__class__.__name__
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
-        accu = accuracy_score(y_test,y_pred)
-        prec = precision_score(y_test, y_pred)
-        rec = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+        accu = accuracy_score(y_test, y_pred)
+        prec = precision_score(y_test, y_pred, average='weighted')
+        rec = recall_score(y_test, y_pred, average='weighted')
+        f1 = f1_score(y_test, y_pred, average='weighted')
         if name in acc_dict:
             acc_dict[name] += accu
             prec_dict[name] += prec
@@ -98,25 +101,26 @@ for train_index, test_index in skfolds.split(X, y):
             prec_dict[name] = prec
             rec_dict[name] = rec
             f1_dict[name] = f1
-            
+
 for clf in acc_dict:
-    acc_dict[clf] = acc_dict[clf]/10
-    prec_dict[clf] = prec_dict[clf]/10
-    rec_dict[clf] = rec_dict[clf]/10
-    f1_dict[clf] = f1_dict[clf]/10
-    new_entry = pd.DataFrame([[clf, acc_dict[clf], prec_dict[clf], rec_dict[clf], f1_dict[clf]]], columns = cols)
-    log = log.append(new_entry, ignore_index = True)
-    
+    acc_dict[clf] = acc_dict[clf] / 10
+    prec_dict[clf] = prec_dict[clf] / 10
+    rec_dict[clf] = rec_dict[clf] / 10
+    f1_dict[clf] = f1_dict[clf] / 10
+    new_entry = pd.DataFrame([[clf, acc_dict[clf], prec_dict[clf],
+                               rec_dict[clf], f1_dict[clf]]],
+                             columns=cols)
+    log = log.append(new_entry, ignore_index=True)
+
 barWidth = 0.2
 r1 = np.arange(len(log.Classifier))
 r2 = [x + barWidth for x in r1]
 r3 = [x + barWidth for x in r2]
 r4 = [x + barWidth for x in r3]
 
-fig, ax = plt.subplots(figsize = (15,10))
-plt.bar(r1, log.Accuracy, width = 0.2)
-plt.bar(r2, log.Precision, width = 0.2)
-plt.bar(r3, log.Recall, width = 0.2)
-plt.bar(r4, log.F1Score, width = 0.2)
+fig, ax = plt.subplots(figsize=(15, 10))
+plt.bar(r1, log.Accuracy, width=0.2)
+plt.bar(r2, log.Precision, width=0.2)
+plt.bar(r3, log.Recall, width=0.2)
+plt.bar(r4, log.F1Score, width=0.2)
 plt.grid()
-# Load the data
